a5d230f2921b1854059b7673b9f9aa61d332382c
udpsec <udpsec@users.noreply.github.com>
2019-01-15 09:51:50 +0800

    update

--- src/main/java/com/inofd/ssm/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.inofd.ssm.service;
-
-import com.inofd.ssm.entity.User;
-import org.springframework.stereotype.Service;
-
-public interface UserService  {
-  public   User selectUserById(Integer userId);
-  public  void  insertUser(User user);
-}
---
+++ src/main/java/com/inofd/ssm/web/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.inofd.ssm.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(value = "hello",urlPatterns = "/hello.view")
+class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf8");
+        resp.setContentType("text/html;charset=utf8");
+        PrintWriter out = new PrintWriter(System.out);
+        out.println("hello");
+    }
+}
